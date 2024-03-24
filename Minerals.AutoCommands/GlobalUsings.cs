e75a8d61c737a05c3d883c0bb537378d763f3e38
Szymon Hałucha <99204426+SzymonHalucha@users.noreply.github.com>
2024-03-24 08:42:25 +0100

    Changed code building system

--- Minerals.AutoCommands/GlobalUsings.cs
+++ Minerals.AutoCommands/GlobalUsings.cs
@@ -1,6 +1,7 @@
 global using System;
 global using System.Linq;
 global using System.Text;
+global using System.Reflection;
 global using System.Collections.Generic;
 global using System.Collections.Immutable;
 global using System.Runtime.CompilerServices;
---
+++ Minerals.AutoCommands/README.md
@@ -0,0 +1,2 @@
+# Minerals.AutoCommands
+Package that uses incremental generators to provide useful attributes for creating CLI tools for dotnet platform
